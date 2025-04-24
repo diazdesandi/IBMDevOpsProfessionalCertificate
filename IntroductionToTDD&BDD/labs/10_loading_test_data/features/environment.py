d1f3a9d86bdf733a991d8a1456b24efedfe2d704
rene <diaz.de.sandi.7@gmail.com>
2025-04-23 22:50:53 -0700

    Refactor Behave testing environment by removing Firefox WebDriver setup

--- IntroductionToTDD&BDD/labs/10_loading_test_data/features/environment.py
+++ IntroductionToTDD&BDD/labs/10_loading_test_data/features/environment.py
@@ -2,27 +2,15 @@
 Environment for Behave Testing
 """
 from os import getenv
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
 BASE_URL = getenv('BASE_URL', 'http://localhost:8080')
-
 
 def before_all(context):
     """ Executed once before all tests """
     context.base_url = BASE_URL
     context.wait_seconds = WAIT_SECONDS
-    # Instantiate the Firefox WebDriver with GeckoDriver
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    service = FirefoxService(GeckoDriverManager().install())
-    context.driver = webdriver.Firefox(options=options)
-    context.driver.implicitly_wait(context.wait_seconds)
 
 def after_all(context):
     """ Executed after all tests """
-    if hasattr(context, 'driver'):
-        context.driver.quit()
+    pass
